eafc1bfd2bf8dffdba16dbd53441a6e4f7c9d431
VWGomes <werneckvictor@hotmail.com>
2021-09-17 12:29:43 -0300

    Create Horario do jogo de xadrez.cs
    Horário de jogo de xadrez

---
+++ Verificar se é maior que 10.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{ 
+    class Program 
+    { 
+        static void Main(string[] args) 
+        { 
+            int numero = int.Parse(Console.ReadLine()); 
+            if (numero > 10) 
+            { Console.WriteLine("É MAIOR QUE 10!"); 
+            } else 
+            { Console.WriteLine("NÃO É MAIOR QUE 10!"); 
+            } Console.ReadLine(); 
+        } 
+    } 
+}
