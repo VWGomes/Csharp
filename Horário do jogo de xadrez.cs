dc00fcf8dae95fda9f0422c7c6394093f29949d5
VWGomes <88067055+VWGomes@users.noreply.github.com>
2021-09-18 12:58:33 -0300

    Add files via upload

---
+++ Horário do jogo de xadrez.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp2
+{ 
+    class Program 
+    { static void Main(string[] args) 
+        { 
+            int inicio; 
+            int fim; 
+            int total; 
+            
+            Console.WriteLine("Qual o horário do início do jogo?"); 
+            
+            inicio = Convert.ToInt32(Console.ReadLine()); 
+            
+            Console.WriteLine("Qual o horário de término do jogo?"); 
+            
+            fim = Convert.ToInt32(Console.ReadLine()); if (fim < inicio)             
+            {             
+                total = (24 - inicio) + fim;             
+            }             
+            else             
+            {             
+                total = fim - inicio;             
+            }             
+            Console.WriteLine("O total de horas do jogo de xadrez foi:"); 
+            
+            Console.Write(total); 
+            
+            Console.ReadKey();         
+        }     
+    } 
+}
