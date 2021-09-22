3882d28a4c71e4fdd098fcbd57a6d42640f808f4
VWGomes <88067055+VWGomes@users.noreply.github.com>
2021-09-22 17:18:20 -0300

    Par ou impar

---
+++ par impar.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: user
+ * Date: 22/09/2021
+ * Time: 17:13
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Projet
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			int NumeroDigitado;
+
+            Console.Write("Insira um número: "); 
+
+            NumeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+            if (NumeroDigitado % 2 == 0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
