b65847833963b82a76433cfbd05878c53fb19015
Victor Werneck <88067055+VWGomes@users.noreply.github.com>
2021-10-27 19:21:19 -0300

    Add files via upload

---
+++ Desafio Array.cs
@@ -0,0 +1,25 @@
+class Program 
+{
+  static void Main(string[] args) 
+  
+  { 
+
+    {
+
+      string[] produtos = new string[6] 
+      
+      {
+        "FIFA",
+        "Need for Speed",
+        "The Sims",
+        "CSGO",
+        "Dead By Daylight",
+        "Team Fortress"   
+      }
+
+      Console.WriteLine(produtos[0,1,2,3,4,5])
+
+      Console.ReadLine();
+    }
+  }
+}
