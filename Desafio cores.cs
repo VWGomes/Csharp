31a9b4b3860339db96d11d41d03b91799b4cdfb4
Victor Werneck <88067055+VWGomes@users.noreply.github.com>
2021-11-06 14:11:31 -0300

    Add files via upload

---
+++ Desafio cores.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+	
+class program
+	
+{
+	static void Main(string[] args)
+	{
+		
+		string cor = "Verde";
+	
+		switch(cor){
+			
+				case "Vermelho";
+				
+				Console.WriteLine("Sua cor é vermelho.");
+			
+	}
+	
+	Console.Readline();
+}
