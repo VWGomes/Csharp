1b0c2df83d22f7f2140fffb3710e5fcfea7864c1
Victor Werneck <88067055+VWGomes@users.noreply.github.com>
2021-11-19 16:45:09 -0300

    Add files via upload

---
+++ Desafio vetor.cs
@@ -0,0 +1,35 @@
+﻿#  Desafio
+#Você recebeu o desafio de ler um valor e criar um programa que coloque o valor lido na primeira posição de um vetor N[10]. Em cada posição subsequente, coloque o dobro do valor da posição anterior. Por exemplo, se o valor lido for 1, os valores do vetor devem ser 1,2,4,8 e assim sucessivamente. Mostre o vetor em seguida.
+#Entrada
+#A entrada contém um valor inteiro (V<=50).
+#Saída
+#Para cada posição do vetor, escreva "N[i] = X", onde i é a posição do vetor e X é o valor armazenado na posição i. O primeiro número do vetor N (N[0]) irá receber o valor de V.
+
+
+
+using System;
+
+class Program {
+
+    static void Main(string[] args) { 
+
+            int V = int.Parse(Console.ReadLine());
+
+            int[] N = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 0)
+                {
+                    N[i] = V;
+                }
+                else
+                {
+                    N[i] = N[i - 1] * 2;
+                }
+
+                Console.WriteLine("N[" + i + "] = " + N[i]);
+                }
+           } 
+
+}
